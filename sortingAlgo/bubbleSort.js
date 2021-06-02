44470431f6972639fc90bccd06bedd7c621a7ac6
rjOwl <mostafayossef100@yahoo.com>
2021-06-02 02:07:45 +0200

    Changed colors of bubbleSort and removed comments from selectionSort

--- sortingAlgo/bubbleSort.js
+++ sortingAlgo/bubbleSort.js
@@ -1,22 +1,21 @@
 function bubbleSort(heightsArr, bars) {
     let cpArr = [...heightsArr]
     for (let i = 0; i < heightsArr.length - 1; i++) {
-        for (let j = 0; j < heightsArr.length - 1; j++) {
+        for (let j = 0; j < heightsArr.length - i- 1; j++) {
+            updateStyle(bars[j], heightsArr[j], colors.comparing)
+            updateStyle(bars[j + 1], heightsArr[j + 1], colors.comparing)
             if (heightsArr[j] > heightsArr[j + 1]) {
-                updateStyle(bars[j], heightsArr[j], 'red')
-                updateStyle(bars[j + 1], heightsArr[j + 1], 'red')
+                updateStyle(bars[j], heightsArr[j], colors.swaped)
+                updateStyle(bars[j + 1], heightsArr[j + 1], colors.swaped)
                 var temp = heightsArr[j];
                 heightsArr[j] = heightsArr[j + 1]
                 heightsArr[j + 1] = temp
-                updateStyle(bars[j], heightsArr[j], 'red')
-                updateStyle(bars[j + 1], heightsArr[j + 1], 'red')
+                updateStyle(bars[j], heightsArr[j], colors.swaped)
+                updateStyle(bars[j + 1], heightsArr[j + 1], colors.swaped)
             }
             updateStyle(bars[j], heightsArr[j], colors.default)
             updateStyle(bars[j + 1], heightsArr[j + 1], colors.default)
         }
+        updateStyle(bars[i], heightsArr[i], colors.default)
     }
 }
-let delay = 1
-
-// setTimeout(() => bubbleSort(barsHeightArr, bars), 1000)
-
--- sortingAlgo/selectionSort.js
+++ sortingAlgo/selectionSort.js
@@ -1,22 +1,14 @@
-let x_cor = []
-let o = 1
-
 function selectionSortAnimation(dataArr, bars){
     for(let i = 0; i <dataArr.length-1; i++){
         minIndex = i;
         updateStyle(bars[i], dataArr[i], colors.comparing)
-        // bars[i].setAttribute("fill", colors.comparing)
         for(let j = i+1; j <dataArr.length; j++){
             updateStyle(bars[j], dataArr[j], colors.found)
-            // bars[j].setAttribute("fill", colors.comparing)
             if(dataArr[j] < dataArr[minIndex]){
-                // updateStyle(bars[i], dataArr[i], colors.found)
                 updateStyle(bars[j], dataArr[j], colors.black)
                 if(minIndex!=i)
                     updateStyle(bars[minIndex], dataArr[minIndex], colors.default)
                 minIndex = j
-                // bars[i].setAttribute("fill", colors.found)
-                // bars[j].setAttribute("fill", colors.found)
             }
             else
                 updateStyle(bars[j], dataArr[j], colors.default)
@@ -24,33 +16,11 @@ function selectionSortAnimation(dataArr, bars){
         if(minIndex != i){
             updateStyle(bars[i], dataArr[i], colors.swaped);
             updateStyle(bars[minIndex], dataArr[minIndex], colors.swaped);
-            // bars[i].setAttribute("fill", colors.swaped);
-            // bars[minIndex].setAttribute("fill", colors.swaped);
             [dataArr[i], dataArr[minIndex]] = [dataArr[minIndex], dataArr[i]];
-
-            // [bars[i], bars[minIndex]] = [bars[minIndex], bars[i]];
-            // from = bars[i].getAttribute("x")
-            // to = bars[minIndex].getAttribute("x")
-            // x_cor.push([
-            //     { ele: bars[i],         from: to,   to:(i+1)*X},
-            //     { ele: bars[minIndex],  from: from, to:X*(minIndex+1)}
-            // ])
             updateStyle(bars[i], dataArr[i], colors.swaped)
             updateStyle(bars[minIndex], dataArr[minIndex], colors.swaped)
-
         }
         updateStyle(bars[i], dataArr[i], colors.default)
         updateStyle(bars[minIndex], dataArr[minIndex], colors.default)
-        // else{
-            // bars[minIndex].setAttribute("fill", colors.default)
-            // bars[i].setAttribute("fill", colors.default)
-        // }                    
     }
-    // while(x_cor.length){
-    //     const [bigBar, smallBar] = x_cor.shift()
-    //     setTimeout(()=>{
-    //         cretateAnimation(bigBar, smallBar)
-    //     }, (o+1)*300)
-    //     o++
-    // }
 }
